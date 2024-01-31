2e41e1c12b594a9f4daa05fa2fb4fba0eb8deabd
ndri_menan <menanpatrick49@gmail.com>
2024-01-31 11:10:34 +0100

    première itération

--- src/Main.py
+++ src/Main.py
@@ -1,12 +1,39 @@
 # coding: utf-8
 import os
 from io import StringIO
+import random
 
 
 #Lire le fichier principal
 path = 'D:/git_repo/split_big_file/data/M50_TEST.TXT'
+tmp_file_path = 'D:/git_repo/split_big_file/data/tmp_file.txt'
+entete = ''
+pied_page = ''
 file = open(path, 'r')
-print(file.read())
-#file_buffer = StringIO( file.read())
-#print(file_buffer.getvalue())
+file_buffer = StringIO(file.read()) 
+file_tmp = open(tmp_file_path, 'x')
+for line in file_buffer:
+    if line.startswith('00.00'):
+        entete = line
+    elif line.startswith('99.00'):
+        pied_page = line
+    else:
+        file_tmp.write(line)
+
+file_tmp.close()
+file_tmp = open(tmp_file_path, 'r')
+file_tmp_buffer = StringIO(file_tmp.read())   
+split_data = file_tmp_buffer.getvalue().split('50.002')
+#print(entete)
+#print(pied_page)
+#print(len(split_data))
+for data in range(1, len(split_data)):
+    random_number = random.randint(100, 200)
+    path_split = 'D:/git_repo/split_big_file/data/file_splitted/split_'+str(random_number)+".txt"
+    file = open(path_split, 'x')
+    file.write(entete)
+    file.write(split_data[data])
+    file.write(pied_page)
+    file.close()
+
 file.close
